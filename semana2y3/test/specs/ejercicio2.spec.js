1e9a649999b879433028925f6520058d77aec9a6
pablo <pjcalvov@gmail.com>
2021-06-03 20:03:44 -0600

    timeouts

--- semana2y3/test/specs/ejercicio2.spec.js
+++ semana2y3/test/specs/ejercicio2.spec.js
@@ -5,14 +5,18 @@ describe('EJERCICIO2 Find my benefits',() => {
 
     before(async ()=> {
         // vaya al home page
-        await browser.url('/');
+        
     });
+
+    beforeEach( async () => {
+        await browser.reloadSession();
+        await browser.url('/');
+    })
 
     it('should be able to scroll the page', async () => {  
         console.log('in NODE')
         await browser.execute( () => window.scrollBy(0, 100));
         await browser.execute( () => console.log('IN BROWSER'));
-
         await browser.pause(3000);
 
         const el = await $('[data-selenium-id=lnkSignIn]')
@@ -23,5 +27,21 @@ describe('EJERCICIO2 Find my benefits',() => {
         await browser.pause(3000);
 
     })
+
+    it('refresh the page', async () => { 
+        await browser.pause(4000);
+    });
+
+    it('set timeouts', async () => { 
+        await browser.setTimeout({implicit: 1000});
+        await $('adsadasdas');
+    });
+
+    it('set timeouts', async () => { 
+        await browser.setTimeout({pageLoad: 1000});
+        await browser.url('https://valleycare.com');
+        await browser.setTimeout({implicit: 30000});
+    });
+    
 
 });
