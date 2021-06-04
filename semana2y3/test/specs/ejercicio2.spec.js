371f114fedd502480880324fe8704adf5d2d2a20
pablo <pjcalvov@gmail.com>
2021-06-03 20:48:10 -0600

    clase terminada

--- semana2y3/test/specs/ejercicio2.spec.js
+++ semana2y3/test/specs/ejercicio2.spec.js
@@ -4,8 +4,23 @@ const VisionInsurance = require('../pageobjects/vision-insurance.page');
 describe('EJERCICIO2 Find my benefits',() => {
 
     before(async ()=> {
-        // vaya al home page
-        
+
+        const a = '';
+
+    browser.addCommand('jsClick', 
+            async function () {
+                await this.execute( 
+                    (elemento) => elemento.click(), 
+                    this );
+    }, true);
+
+    browser.addCommand('getUrlAndTitle', async function () {
+        // `this` refers to the `browser` scope
+        return {
+            url: await this.getUrl(),
+            title: await this.getTitle()
+        }
+        })
     });
 
     beforeEach( async () => {
@@ -18,14 +33,10 @@ describe('EJERCICIO2 Find my benefits',() => {
         await browser.execute( () => window.scrollBy(0, 100));
         await browser.execute( () => console.log('IN BROWSER'));
         await browser.pause(3000);
-
         const el = await $('[data-selenium-id=lnkSignIn]')
         await browser.execute( (elemento) => elemento.click(), el );
-
         await browser.execute( ()=> document.getElementById('newFindYourBrandButton').remove());
-
         await browser.pause(3000);
-
     })
 
     it('refresh the page', async () => { 
@@ -41,6 +52,32 @@ describe('EJERCICIO2 Find my benefits',() => {
         await browser.setTimeout({pageLoad: 1000});
         await browser.url('https://valleycare.com');
         await browser.setTimeout({implicit: 30000});
+    });
+
+    it('waits', async () => { 
+        await browser.pause(20000);
+        const el = await $('[data-selenium-id=lnkSignIn]');
+        await browser.execute((elemento) => elemento.click(), el );
+
+        await(await $('#newFindYourBrandButton')).waitForClickable({timeout:3000});
+    });
+
+    it('waitUntil', async () => {
+        
+        const elem = await $('[data-selenium-id=lnkSignIn]');
+        await elem.waitUntil(async function () {
+            return await this.getText() === 'Sign In'
+        }, {
+            timeout: 5000,
+            timeoutMsg: 'expected text to be different after 5s'
+        });
+    });
+
+    it.only('customCommand', async () => {
+        const elem = await $('[data-selenium-id=lnkSignIn]');
+        await elem.jsClick();
+
+        console.log(await browser.getUrlAndTitle());
     });
     
 
